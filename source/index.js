9ffb517e6ba601644bec4c00f7d56cbfadce4457
Pacifique Nshuti <pacific.nsuti@zatechub.com>
2022-10-03 11:12:28 +0200

    Initial Commit

---
+++ source/index.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                devlitesPink: '#d81754',
+                devlitesPinkDark: '#aa1242',
+                devlitesGray: '#111522',
+                devlitesText: '#333333',
+                devlitesLightGray: '#222a41',
+                devlitesWhiteLight: '#bbbbbb'
+            },
+            fontFamily: {
+                sans: ['Raleway', 'sans-serif']
+            }
+        }
+    }
+}
